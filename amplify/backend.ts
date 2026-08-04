c8d97eec02e4a1a538a22b074a99e5801e3b4d00
An Tran <mercury02061612@gmail.com>
2026-08-04 02:50:33 -0700

    Login Successfully

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,21 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
+// import { data } from './data/resource'; Delete comment to enable data storage and API endpoints
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
-  data,
 });
+
+// Disable public self-registration.
+// Only an administrator can create test users.
+// Delete this code to allow public self-registration of users.
+
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+
+cfnUserPool.adminCreateUserConfig = {
+  allowAdminCreateUserOnly: true,
+};
+
